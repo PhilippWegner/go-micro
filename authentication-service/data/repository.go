bd43e276af8279085d678716066205ff7b767dfa
PhilippWegner <philipp.wegner@stud.hs-bochum.de>
2023-11-08 17:49:35 +0100

    go micro

---
+++ authentication-service/data/repository.go
@@ -0,0 +1,11 @@
+package data
+
+type Repository interface {
+	GetAll() ([]*User, error)
+	GetByEmail(email string) (*User, error)
+	Update(user User) error
+	DeleteByID(id int) error
+	Insert(user User) (int, error)
+	ResetPassword(password string, user User) error
+	PasswordMatches(plainText string, user User) (bool, error)
+}
